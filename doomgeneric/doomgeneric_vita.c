5b85792513f8efea57e50cf3b7c5e04da304a4d8
agatti4 <ajgatti4@gmail.com>
2026-01-12 21:36:19 -0500

    Make controls act correctly

--- doomgeneric/doomgeneric_vita.c
+++ doomgeneric/doomgeneric_vita.c
@@ -20,6 +20,15 @@ SDL_Texture* texture;
 #define WINDOW_RESX 960
 #define WINDOW_RESY 544
 
+#define KEYQUEUE_SIZE 16
+static unsigned short s_KeyQueue[KEYQUEUE_SIZE];
+static unsigned int s_KeyQueueWriteIndex = 0;
+static unsigned int s_KeyQueueReadIndex = 0;
+
+static unsigned int old_buttons = 0;
+static int old_lx = 128, old_ly = 128;
+static int old_rx = 128;
+
 #define STICK_DEADZONE 32
 
 static int stick_left(int v)  { return v < (128 - STICK_DEADZONE); }
@@ -47,23 +56,92 @@ void DG_Init(){
   texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGB888, SDL_TEXTUREACCESS_STREAMING, DOOMGENERIC_RESX, DOOMGENERIC_RESY);
 }
 
+static void addKeyToQueue(int pressed, unsigned int keyCode){
+  unsigned short keyData = (pressed << 8) | keyCode;
+
+  s_KeyQueue[s_KeyQueueWriteIndex] = keyData;
+  s_KeyQueueWriteIndex++;
+  s_KeyQueueWriteIndex %= KEYQUEUE_SIZE;
+}
+
+static void Vita_HandleKeyInput()
+{
+    SceCtrlData ctrl;
+    sceCtrlPeekBufferPositive(0, &ctrl, 1);
+
+    unsigned int changed = ctrl.buttons ^ old_buttons;
+    unsigned int pressed = changed & ctrl.buttons;
+    unsigned int released = changed & old_buttons;
+
+    // Only queue newly pressed buttons
+    if (pressed & SCE_CTRL_CROSS)    addKeyToQueue(1, KEY_ENTER);
+    if (pressed & SCE_CTRL_CIRCLE)   addKeyToQueue(1, KEY_USE);
+    if (pressed & SCE_CTRL_SQUARE)   addKeyToQueue(1, KEY_RSHIFT);
+    if (pressed & SCE_CTRL_TRIANGLE) addKeyToQueue(1, KEY_TAB);
+    if (pressed & SCE_CTRL_START)    addKeyToQueue(1, KEY_ESCAPE);
+    if (pressed & SCE_CTRL_SELECT)   addKeyToQueue(1, KEY_F1);
+    if (pressed & SCE_CTRL_RTRIGGER) addKeyToQueue(1, KEY_FIRE);
+    if (pressed & SCE_CTRL_LTRIGGER) addKeyToQueue(1, KEY_USE);
+
+    // Only queue releases if needed
+    if (released & SCE_CTRL_CROSS)    addKeyToQueue(0, KEY_ENTER);
+    if (released & SCE_CTRL_RTRIGGER) addKeyToQueue(0, KEY_FIRE);
+    // add for other buttons if needed
+
+    old_buttons = ctrl.buttons;
+
+    // ----- LEFT STICK (MOVE) -----
+    if (stick_left(ctrl.ly) != stick_left(old_ly)) {
+        addKeyToQueue(stick_left(ctrl.ly), KEY_UPARROW);
+        old_ly = ctrl.ly;
+    }
+    if (stick_right(ctrl.ly) != stick_right(old_ly)) {
+        addKeyToQueue(stick_right(ctrl.ly), KEY_DOWNARROW);
+        old_ly = ctrl.ly;
+    }
+    if (stick_left(ctrl.lx) != stick_left(old_lx)) {
+        addKeyToQueue(stick_left(ctrl.lx), KEY_LEFTARROW);
+        old_lx = ctrl.lx;
+    }
+    if (stick_right(ctrl.lx) != stick_right(old_lx)) {
+        addKeyToQueue(stick_right(ctrl.lx), KEY_RIGHTARROW);
+        old_lx = ctrl.lx;
+    }
+
+    // ----- RIGHT STICK (TURN) -----
+    if (stick_left(ctrl.rx) != stick_left(old_rx)) {
+        addKeyToQueue(stick_left(ctrl.rx), KEY_LEFTARROW);
+        old_rx = ctrl.rx;
+    }
+    if (stick_right(ctrl.rx) != stick_right(old_rx)) {
+        addKeyToQueue(stick_right(ctrl.rx), KEY_RIGHTARROW);
+        old_rx = ctrl.rx;
+    }
+}
+
 void DG_DrawFrame()
 {
   SDL_UpdateTexture(texture, NULL, DG_ScreenBuffer, DOOMGENERIC_RESX * sizeof(uint32_t));
 
   SDL_RenderClear(renderer);
 
+  // Optional: preserve aspect ratio with letterbox
+#ifdef NO_STRETCH
+  int scale = WINDOW_RESY / DOOMGENERIC_RESY; // 544 / 200 = 2
+  int w = DOOMGENERIC_RESX * scale;
+  int h = DOOMGENERIC_RESY * scale;
+  SDL_Rect dst = { (WINDOW_RESX - w)/2, (WINDOW_RESY - h)/2, w, h };
+  SDL_RenderCopy(renderer, texture, NULL, &dst);
+#else
   // Scale the Doom texture to fit the Vita screen
   SDL_Rect dst = { 0, 0, WINDOW_RESX, WINDOW_RESY };
+    SDL_RenderCopy(renderer, texture, NULL, NULL);
+#endif
 
-  // Optional: preserve aspect ratio with letterbox
-  // int scale = WINDOW_RESY / DOOMGENERIC_RESY; // 544 / 200 = 2
-  // int w = DOOMGENERIC_RESX * scale;
-  // int h = DOOMGENERIC_RESY * scale;
-  // SDL_Rect dst = { (WINDOW_RESX - w)/2, (WINDOW_RESY - h)/2, w, h };
-
-  SDL_RenderCopy(renderer, texture, NULL, NULL);
   SDL_RenderPresent(renderer);
+
+  // Poll Vita buttons and queue them
+  Vita_HandleKeyInput();
 }
 
 void DG_SleepMs(uint32_t ms)
@@ -76,64 +154,23 @@ uint32_t DG_GetTicksMs()
   return SDL_GetTicks();
 }
 
-int DG_GetKey(int *pressed, unsigned char *key)
+int DG_GetKey(int* pressed, unsigned char* doomKey)
 {
-    static unsigned int old_buttons = 0;
-    static int old_lx = 128, old_ly = 128; // left stick
-    static int old_rx = 128;               // right stick
-
-    SceCtrlData ctrl;
-    sceCtrlPeekBufferPositive(0, &ctrl, 1);
-
-    // ----- DIGITAL BUTTONS -----
-    unsigned int changed = ctrl.buttons ^ old_buttons;
-
-    unsigned int mask = 1;
-    for (int i = 0; i < 32; i++, mask <<= 1)
-    {
-        if (changed & mask)
-        {
-            *pressed = (ctrl.buttons & mask) != 0;
-
-            switch (mask)
-            {
-                case SCE_CTRL_CROSS:     *key = KEY_ENTER;  return 1; // Confirm
-                case SCE_CTRL_CIRCLE:    *key = KEY_USE;    return 1; // Alt use
-                case SCE_CTRL_SQUARE:    *key = KEY_RSHIFT; return 1; // Run
-                case SCE_CTRL_TRIANGLE:  *key = KEY_TAB;    return 1; // Map / Tab
-                case SCE_CTRL_START:     *key = KEY_ESCAPE; return 1; // Pause / menu
-                case SCE_CTRL_SELECT:    *key = KEY_F1;     return 1; // Optional / HUD
-                case SCE_CTRL_RTRIGGER:  *key = KEY_FIRE;   return 1; // Fire
-                case SCE_CTRL_LTRIGGER:  *key = KEY_USE;    return 1; // Use / interact
-                case SCE_CTRL_UP:        *key = KEY_UPARROW; return 1;
-                case SCE_CTRL_DOWN:      *key = KEY_DOWNARROW; return 1;
-                case SCE_CTRL_LEFT:      *key = KEY_LEFTARROW; return 1;
-                case SCE_CTRL_RIGHT:     *key = KEY_RIGHTARROW; return 1;
-            }
-        }
-    }
-
-    old_buttons = ctrl.buttons;
-
-    // ----- LEFT STICK (MOVE) -----
-    int move_up    = ctrl.ly < 128 - STICK_DEADZONE;
-    int move_down  = ctrl.ly > 128 + STICK_DEADZONE;
-    int move_left  = ctrl.lx < 128 - STICK_DEADZONE;
-    int move_right = ctrl.lx > 128 + STICK_DEADZONE;
-
-    if (move_up    != (old_ly < 128 - STICK_DEADZONE))    { *pressed = move_up;    *key = KEY_UPARROW; old_ly = ctrl.ly; return 1; }
-    if (move_down  != (old_ly > 128 + STICK_DEADZONE))    { *pressed = move_down;  *key = KEY_DOWNARROW; old_ly = ctrl.ly; return 1; }
-    if (move_left  != (old_lx < 128 - STICK_DEADZONE))    { *pressed = move_left;  *key = KEY_LEFTARROW; old_lx = ctrl.lx; return 1; }
-    if (move_right != (old_lx > 128 + STICK_DEADZONE))    { *pressed = move_right; *key = KEY_RIGHTARROW; old_lx = ctrl.lx; return 1; }
-
-    // ----- RIGHT STICK (TURN) -----
-    int turn_left  = ctrl.rx < 128 - STICK_DEADZONE;
-    int turn_right = ctrl.rx > 128 + STICK_DEADZONE;
-
-    if (turn_left  != (old_rx < 128 - STICK_DEADZONE)) { *pressed = turn_left;  *key = KEY_LEFTARROW;  old_rx = ctrl.rx; return 1; }
-    if (turn_right != (old_rx > 128 + STICK_DEADZONE)) { *pressed = turn_right; *key = KEY_RIGHTARROW; old_rx = ctrl.rx; return 1; }
-
+  if (s_KeyQueueReadIndex == s_KeyQueueWriteIndex){
+    //key queue is empty
     return 0;
+  }else{
+    unsigned short keyData = s_KeyQueue[s_KeyQueueReadIndex];
+    s_KeyQueueReadIndex++;
+    s_KeyQueueReadIndex %= KEYQUEUE_SIZE;
+
+    *pressed = keyData >> 8;
+    *doomKey = keyData & 0xFF;
+
+    return 1;
+  }
+
+  return 0;
 }
 
 void DG_SetWindowTitle(const char * title)
